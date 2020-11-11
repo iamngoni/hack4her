c06acbf5c2e8a38c8e111b1825c61f2132ab9472
iamngoni <el.nick.ngonie@gmail.com>
2020-11-11 15:02:21 +0200

    changed package.json

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-require("dotenv").config();
-const express = require("express");
-const logger = require("morgan");
-const bp = require("body-parser");
-const cors = require("cors");
-const path = require("path");
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const bp = require('body-parser');
+const cors = require('cors');
 
-let app = express();
-app.use(logger("dev"));
+const app = express();
+app.use(logger('dev'));
 app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(cors());
+app.use(require('./routes'));
+
+module.exports = app;
